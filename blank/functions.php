2aa385dd27998e0efd9c7f1da80e9e87c70e290e
Tiago Luvizoto Neves <tiagoluvizotoneves@gmail.com>
2024-04-21 17:51:07 -0300

    chore: desenvolvendo site modelo para testes de funcionalidades

--- blank/functions.php
+++ blank/functions.php
@@ -1,9 +1,1 @@
 <?php
-
-function redirect_root_only() {
-    if ($_SERVER['REQUEST_URI'] == '/' || $_SERVER['REQUEST_URI'] == '') {
-        wp_redirect('https://carmensteffens.com.br');
-        exit;
-    }
-}
-add_action('template_redirect', 'redirect_root_only');
